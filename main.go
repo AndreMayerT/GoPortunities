69dbf1bcf5891bff2662f4eed29018416d7ce779
AndreMayerT <andremayerusa@gmail.com>
2023-09-26 16:32:02 -0300

    feat: add logger subpackage

--- main.go
+++ main.go
@@ -1,8 +1,23 @@
 package main
 
-import "github.com/AndreMayerT/GoPortunities/router"
+import (
+	"github.com/AndreMayerT/GoPortunities/config"
+	"github.com/AndreMayerT/GoPortunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+
+	// Initialize Configs
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 
 	// Initialize Router
 	router.Initialize()
